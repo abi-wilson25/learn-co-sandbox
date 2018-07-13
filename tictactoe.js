aa4e3cc19f5af7403e6aa9c4109bbb3d42005f16
abi-wilson25 <agibailw@gmail.com>
2018-07-13 19:06:53 +0000

    automatically backed up by learn

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,11 @@
 let winArr = [
-  ["#0-0", "#1-0", "#2-0"], // top row
+  ["#0-0", "#1-0", "#2-0"],
+  ["#0-1", "#1-1", "#2-1"],
+  ["#0-2", "#1-2", "#2-2"]
+  
+  
+  
+  // top row
 //continue untill all rows are filled
 	
 let mark =	
