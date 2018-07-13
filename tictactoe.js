1e16ce69232e35b24ac5644949ff2aff79fe079a
abi-wilson25 <agibailw@gmail.com>
2018-07-13 19:33:42 +0000

    automatically backed up by learn

--- tictactoe.js
+++ tictactoe.js
@@ -25,7 +25,7 @@ function markCell(){
   if (!this.innerText) {
     this.innerText = mark
     if (playerWon(mark)){
-      console.log(mark, "You won the game")
+      alert((mark, "You won the game"))
     }
   mark = (mark === 'X') ? 'O' : 'X'  
   }
