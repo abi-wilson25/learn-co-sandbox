a8c62fd2800691f46f2948bea0f37859541c3574
abi-wilson25 <agibailw@gmail.com>
2018-07-13 19:03:17 +0000

    automatically backed up by learn

---
+++ tictactoe.js
@@ -0,0 +1,21 @@
+let winArr = [
+  ["#0-0", "#1-0", "#2-0"], // top row
+//continue untill all rows are filled
+	
+let mark =	
+	
+//create an event listener that works as a function for when players are marking
+	
+function elementContains(id, mark) {
+  return $(id).text() === mark
+}
+
+// this function runs after each turn
+// it checks whether a given (either 'x' or 'o') populates all
+// three spots in any given win combination
+function playerWon () {
+  for (let i = ; i < ; i) {
+    let winCombo = 
+    let won = w
+    // checks if elementContains returns true for every id
+    if (won) return 
